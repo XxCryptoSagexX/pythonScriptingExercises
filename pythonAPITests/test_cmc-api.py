d67461e198209059babd1d21f22f9702a11be243
XxCryptoSagexX <tre.williams001@gmail.com>
2023-10-12 18:14:58 -0500

    CMC Update

--- pythonAPITests/test_cmc-api.py
+++ pythonAPITests/test_cmc-api.py
@@ -1,10 +1,30 @@
-# RTFM (Read The Documentation https://coinmarketcap.com/api/documentation/v1/#)
+import requests
+from requests import Session
+import secrets
+from pprint import pprint as pp
 
+# https://coinmarketcap.com/api/documentation/v1/#
 
-#Import modules and API Key
+# Test a basic Request
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+headers = { 'Accepts': 'application/json','X-CMC_PRO_API_KEY': secrets.API_KEY, }
 
-
-# Test a basic Reqeust
+r = requests.get(url, headers=headers) 
 
 
 # Build up a class so we can easily make the REST API callss
+class CMC:
+    def __init__(self, token):
+        self.apiurl = 'https://pro-api.coinmarketcap.com'
+        self.headers = { 'Accepts': 'application/json','X-CMC_PRO_API_KEY': token, }
+        self.session = Session()
+        self.session.headers.update(self.headers)
+    
+    def getAllCoins(self):
+        url = self.apiurl + '/v1/cryptocurrency/map'
+        r = self.session.get(url)
+        data = r.json['data']
+        return data
+
+cmc = CMC(secrets.API_KEY)
+pp(cmc.getAllCoins())
