edfac3cab24c6de26dd5074b8133acb3cbe58b47
XxCryptoSagexX <tre.williams001@gmail.com>
2023-10-12 19:04:12 -0500

    Code Cleanup

--- pythonAPITests/test_cmc-api.py
+++ pythonAPITests/test_cmc-api.py
@@ -3,17 +3,17 @@ from requests import Session
 import secrets
 from pprint import pprint as pp
 
-# https://coinmarketcap.com/api/documentation/v1/#
+
 
 # Test a basic Request
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
-headers = { 'Accepts': 'application/json','X-CMC_PRO_API_KEY': secrets.API_KEY, }
+# url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/map'
+# headers = { 'Accepts': 'application/json','X-CMC_PRO_API_KEY': secrets.API_KEY, }
+# r = requests.get(url, headers=headers) 
 
-r = requests.get(url, headers=headers) 
 
-
-# Build up a class so we can easily make the REST API callss
+# Build up a class so we can easily make the REST API calls
 class CMC:
+    # https://coinmarketcap.com/api/documentation/v1/#
     def __init__(self, token):
         self.apiurl = 'https://pro-api.coinmarketcap.com'
         self.headers = { 'Accepts': 'application/json','X-CMC_PRO_API_KEY': token, }
@@ -23,8 +23,16 @@ class CMC:
     def getAllCoins(self):
         url = self.apiurl + '/v1/cryptocurrency/map'
         r = self.session.get(url)
-        data = r.json['data']
+        data = r.json()['data']
         return data
 
+    def getPrice(self, symbol):
+        url = self.apiurl + '/v2/cryptocurrency/quotes/latest'
+        # create the dictionary object that will allow for the "symbol" choice to work 
+        parameters = {'symbol': symbol}
+        r = self.session.get(url, params=parameters)
+        data = r.json()['data']
+        return data
+        
 cmc = CMC(secrets.API_KEY)
-pp(cmc.getAllCoins())
+pp(cmc.getPrice('BTC'))
